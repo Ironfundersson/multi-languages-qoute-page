52e4fc04687bd7f5c3b6be4d631a26d80310c4a9
Szegedi Tamás <szegedi.tamas.1203@gmail.com>
2018-05-28 20:50:35 +0200

    the base HTML, CSS and JavaScript codes

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿function hun() {
+  document.getElementById('hun').style.display = 'block';
+  document.getElementById('eng').style.display = 'none';
+  document.getElementById('fr').style.display = 'none';
+}
+
+function eng() {
+  document.getElementById('hun').style.display = 'none';
+  document.getElementById('eng').style.display = 'block';
+  document.getElementById('fr').style.display = 'none';
+}
+
+function fr() {
+  document.getElementById('hun').style.display = 'none';
+  document.getElementById('eng').style.display = 'none';
+  document.getElementById('fr').style.display = 'block';
+}
